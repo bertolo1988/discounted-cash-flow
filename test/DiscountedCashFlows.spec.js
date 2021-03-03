c318aba8f561beec6105888e939b66a7dfa7a3dc
Tiago <tiagobertolo@gmail.com>
2021-03-03 01:00:06 +0000

    Create method to retrieve the growth rate.

--- test/DiscountedCashFlows.spec.js
+++ test/DiscountedCashFlows.spec.js
@@ -1,8 +1,70 @@
 require('should');
 const DiscountedCashFlows = require('../src/DiscountedCashFlows');
 
+
 describe('DiscountedCashFlows', async () => {
-  describe('calculate', () => {
+  describe('getGrowthRateForYear', () => {
+    it('should get 0.1 for all years', () => {
+      const growthRates = [0.1]
+      for (let yearIndex = 0; yearIndex < DiscountedCashFlows.MAX_YEARS; yearIndex++) {
+        const selectedGrowthRate = DiscountedCashFlows.getGrowthRateForYear(growthRates, yearIndex)
+        selectedGrowthRate.should.equal(growthRates[0])
+      }
+    })
+
+    it('should get 0.4 for year 6, index 5', () => {
+      const growthRates = [0.5, 0.4]
+      const selectedGrowthRate = DiscountedCashFlows.getGrowthRateForYear(growthRates, 5)
+      selectedGrowthRate.should.equal(growthRates[1])
+    })
+
+    it('should get 0.5 for year 5, index 4', () => {
+      const growthRates = [0.5, 0.4]
+      const selectedGrowthRate = DiscountedCashFlows.getGrowthRateForYear(growthRates, 4)
+      selectedGrowthRate.should.equal(growthRates[0])
+    })
+
+    it('should get 0.2 for year 10, index 9', () => {
+      const growthRates = [0.5, 0.4, 0.2]
+      const selectedGrowthRate = DiscountedCashFlows.getGrowthRateForYear(growthRates, 9)
+      selectedGrowthRate.should.equal(growthRates[2])
+    })
+
+    it('should get 0.1 for year 1, index 0', () => {
+      const growthRates = [0.1, 0.4, 0.2]
+      const selectedGrowthRate = DiscountedCashFlows.getGrowthRateForYear(growthRates, 0)
+      selectedGrowthRate.should.equal(growthRates[0])
+    })
+
+    it('should retrieve same rate for every 2 years', () => {
+      const growthRates = [0.4, 0.1, 0.7, 0.8, 0.2]
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 0).should.equal(growthRates[0])
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 1).should.equal(growthRates[0])
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 2).should.equal(growthRates[1])
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 3).should.equal(growthRates[1])
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 4).should.equal(growthRates[2])
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 5).should.equal(growthRates[2])
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 6).should.equal(growthRates[3])
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 7).should.equal(growthRates[3])
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 8).should.equal(growthRates[4])
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 9).should.equal(growthRates[4])
+    })
+
+    it('should retrieve last rate if it goes over maximum number of years', () => {
+      const growthRates = [0.4, 0.1, 0.7, 0.8, 0.2]
+      DiscountedCashFlows.getGrowthRateForYear(growthRates, 11).should.equal(growthRates[4])
+    })
+
+    it('should get different rate for every year', () => {
+      const growthRates = Array.from(Array(10).keys())
+      growthRates.map((growthRate, index) => {
+        const selectedGrowthRate = DiscountedCashFlows.getGrowthRateForYear(growthRates, index)
+        selectedGrowthRate.should.be.eql(growthRates[growthRate])
+      })
+    })
+  })
+
+  describe.skip('calculate', () => {
     it('should calculate discounted cash flow', () => {
       let growthRateUntilFive = 0.12;
       let growthRateUntilTen = 0.07;
